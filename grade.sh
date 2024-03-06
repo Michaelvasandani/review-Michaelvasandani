db2a1406f5e6d60d0a343374a95d87b55a1bb689
Michaelvasandani <vasandanimichael@gmail.com>
2024-03-06 14:27:56 -0800

    The changes

--- grade.sh
+++ grade.sh
@@ -8,6 +8,38 @@ mkdir grading-area
 git clone $1 student-submission
 echo 'Finished cloning'
 
+if [[ -f student-submission/ListExamples.java ]]
+then
+    echo "file found"
+else
+    echo "file not found"
+    exit
+fi
+
+cp student-submission/ListExamples.java TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+javac -cp $CPATH *.java
+
+if [[ $? -eq 0 ]];
+then
+    echo "compilation successfull"
+else 
+    echo "compilation was not succesfull"
+fi
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples\ > junit-output.txt
+$(cat junit-output.txt | tail -n 2 | head -n 1) > filtered.txt
+grep -o "/." filtered.text.txt > dots.txt
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+
+echo "Your score is $successes / $tests"
+
+
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
